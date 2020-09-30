ef49ad267a61f31b8fbd557de7eea15e8af19844
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-09-30 15:54:20 -0300

    Up Files

--- TestesUnitarios-05/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios-05/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -5,15 +5,11 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.Date;
-
-import org.junit.Assert;
 import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
@@ -28,13 +24,11 @@ public class LocacaoServiceTest {
 		//Action
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
-		//Verify
-		Assert.assertTrue(service.getClass() == LocacaoService.class);
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 		//ATENTION: Assert.assertThat deprecated on JUnit version 4.13
-		assertThat(5.0, is(equalTo(locacao.getValor())));
+		
+		//Verify
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		
 		
 	}
 
