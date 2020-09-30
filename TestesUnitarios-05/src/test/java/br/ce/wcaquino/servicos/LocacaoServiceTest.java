2d0ee157dae7145b0adfc405bf98269823aa761b
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-09-30 19:51:49 -0300

    Up Files

--- TestesUnitarios-05/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios-05/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,15 +1,19 @@
 package br.ce.wcaquino.servicos;
 
 
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+
+import java.util.Date;
 
 import org.junit.Test;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
 	
@@ -28,7 +32,8 @@ public class LocacaoServiceTest {
 		
 		//Verify
 		assertThat(locacao.getValor(), is(equalTo(5.0)));
-		
+		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
 		
 	}
 
