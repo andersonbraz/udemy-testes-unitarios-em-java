8be5043d0fbdaa232b7478719dae2ec8bd0ac010
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-10-01 07:41:39 -0300

    Up Files

--- TestesUnitarios-07/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios-07/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -7,6 +7,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 import java.util.Date;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -28,17 +29,23 @@ public class LocacaoServiceTest {
 		//Scenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Anderson");
-		Filme filme = new Filme("God Father", 3, 5.0);
+		Filme filme = new Filme("God Father", 0, 5.0);
 		
 		//Action
-		Locacao locacao = service.alugarFilme(usuario, filme);
-		
-		//ATENTION: Assert.assertThat deprecated on JUnit version 4.13
-		
-		//Verify
-		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
-		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		Locacao locacao;
+		try {
+			
+			locacao = service.alugarFilme(usuario, filme);
+			
+			//Verify
+			error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+			error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+			error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
 		
 	}
 
