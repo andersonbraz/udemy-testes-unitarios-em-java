5e207505cb736f5d3d5034376ee635b0654fbb9f
Anderson Braz de Sousa <braz_anderson@msn.com>
2020-09-30 22:45:07 -0300

    Up Files

---
+++ TestesUnitarios-07/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,45 @@
+package br.ce.wcaquino.servicos;
+
+
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.Date;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTest {
+	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
+	
+	@Test
+	public void testLocacaoService() {
+		
+		//Scenario
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Anderson");
+		Filme filme = new Filme("God Father", 3, 5.0);
+		
+		//Action
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//ATENTION: Assert.assertThat deprecated on JUnit version 4.13
+		
+		//Verify
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		
+	}
+
+}
